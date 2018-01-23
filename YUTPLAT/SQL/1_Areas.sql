ab75168390cf51b6927b439127b185c2ce40a913
Matias Ballestero <mballestero@cys.com.ar>
2018-01-23 18:04:15 -0300

    archivos sql

--- YUTPLAT/SQL/1_Areas.sql
+++ YUTPLAT/SQL/1_Areas.sql
@@ -1,3 +1,24 @@
 
 INSERT INTO [dbo].[Area] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion]) 
-VALUES (N'Ingeniería de Software', N'Ingeniería de Software', N'2018-01-19 22:18:00', NULL, NULL)
+VALUES (N'Ingeniería de Software', N'Ingeniería de Software', GETDATE(), NULL, NULL)
+
+INSERT INTO [dbo].[Area] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion]) 
+VALUES (N'Capacitación Tecnológica', N'Capacitación Tecnológica', GETDATE(), NULL, NULL)
+
+INSERT INTO [dbo].[Area] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion]) 
+VALUES (N'Servicios Informáticos de Valor Agregado', N'Servicios Informáticos de Valor Agregado', GETDATE(), NULL, NULL)
+
+INSERT INTO [dbo].[Area] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion]) 
+VALUES (N'RRHH', N'RRHH', GETDATE(), NULL, NULL)
+
+INSERT INTO [dbo].[Area] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion]) 
+VALUES (N'Calidad', N'Calidad', GETDATE(), NULL, NULL)
+
+INSERT INTO [dbo].[Area] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion]) 
+VALUES (N'Administración y Finanzas', N'Administración y Finanzas', GETDATE(), NULL, NULL)
+
+INSERT INTO [dbo].[Area] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion]) 
+VALUES (N'Comercial', N'Comercial', GETDATE(), NULL, NULL)
+
+INSERT INTO [dbo].[Area] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion]) 
+VALUES (N'Desarrollo Comercial', N'Desarrollo Comercial', GETDATE(), NULL, NULL)
--- YUTPLAT/SQL/2_Objetivos.sql
+++ YUTPLAT/SQL/2_Objetivos.sql
@@ -1,10 +1,102 @@
 
 INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
 SELECT
-	N'Medir el progreso de los proyectos en curso en comparación con el progreso planeado para corregir posibles desvíos.', 
-	N'Medir el progreso de los proyectos en curso en comparación con el progreso planeado para corregir posibles desvíos.', 
-	N'2018-01-19 22:18:00', 
-	NULL, 
-	NULL, 
-	Id 
-FROM [dbo].[Area] WHERE Nombre = N'Ingeniería de Software'
+	N'Medir el progreso de los proyectos en curso en comparación con el progreso planeado para corregir posibles desvíos', 
+	N'Medir el progreso de los proyectos en curso en comparación con el progreso planeado para corregir posibles desvíos', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Ingeniería de Software'
+
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Mejorar la eficiencia del método de estimación', 
+	N'Mejorar la eficiencia del método de estimación', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Ingeniería de Software'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Mejorar la eficiencia del testing y desarrollo', 
+	N'Mejorar la eficiencia del testing y desarrollo', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Ingeniería de Software'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Brindar Servicios Informáticos de alto nivel y adecuada satisfacción de los clientes', 
+	N'Brindar Servicios Informáticos de alto nivel y adecuada satisfacción de los clientes', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Capacitación Tecnológica'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Mejorar la eficiencia del reclutamiento', 
+	N'Mejorar la eficiencia del reclutamiento', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Servicios Informáticos de Valor Agregado'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Desalentar la rotación del personal', 
+	N'Desalentar la rotación del personal', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'RRHH'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Aumentar el nivel de instrucción del personal', 
+	N'Aumentar el nivel de instrucción del personal', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'RRHH'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Evaluar el desempeño de los procesos y la implementación de oportunidades de mejora', 
+	N'Evaluar el desempeño de los procesos y la implementación de oportunidades de mejora', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Ingeniería de Software'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Mejorar la gestión de Solicitudes de Acción', 
+	N'Mejorar la gestión de Solicitudes de Acción', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Calidad'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Mejorar la rentabilidad de los proyectos de desarrollo', 
+	N'Mejorar la rentabilidad de los proyectos de desarrollo', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Administración y Finanzas'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Mejorar la eficacia en la selección de Proveedores', 
+	N'Mejorar la eficacia en la selección de Proveedores', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Administración y Finanzas'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Mejorar la gestión de las compras', 
+	N'Mejorar la gestión de las compras', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Administración y Finanzas'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Asegurar una proporción adecuada del presupuesto dedicada a I+D', 
+	N'Asegurar una proporción adecuada del presupuesto dedicada a I+D', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Administración y Finanzas'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Evaluar la estimación del Forecast', 
+	N'Evaluar la estimación del Forecast', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Administración y Finanzas'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Monitorear la solidez económica', 
+	N'Monitorear la solidez económica', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Administración y Finanzas'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Mejorar la satisfacción del cliente', 
+	N'Mejorar la satisfacción del cliente', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Comercial'
+	
+INSERT INTO [dbo].[Objetivo] ([Nombre], [Descripcion], [FechaCreacion], [UltimoUsuarioModifico], [FechaUltimaModificacion], [AreaID]) 
+SELECT
+	N'Monitorear las tareas comerciales', 
+	N'Monitorear las tareas comerciales', 
+	GETDATE(), NULL, NULL, Id FROM [dbo].[Area] WHERE Nombre = N'Desarrollo Comercial'
