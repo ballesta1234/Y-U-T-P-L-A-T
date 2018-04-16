b97867d3cae66147d99c9784ff780a2858208dda
Matias <ballesteromatias@gmail.com>
2018-04-15 21:29:08 -0300

    Se adapta el nombre de las areas de acuerdo a la cantidad de indicadores

--- YUTPLAT/Scripts/Graficos/heatMap.js
+++ YUTPLAT/Scripts/Graficos/heatMap.js
@@ -1,9 +1,12 @@
-﻿$(function () {
+﻿
+var dictionaryIndicadoresPorArea = {};
+
+$(function () {
 
     jQuery('.detalleGrafico').css("visibility", "hidden");
 
     jQuery.noConflict();
-    
+
     var margin = { top: 40, right: 0, bottom: 0, left: 30 };
 
     var heatMap = obtenerHeatMapViewModel();
@@ -11,8 +14,8 @@
     var cantIndicadoresPorArea = contarIndicadoresPorArea(heatMap)
 
     var contentWidth = parseInt(d3.select('#chart').style('width'));
-       
-    var gridSize = 40;    
+
+    var gridSize = 40;
     var height = (gridSize - 8) * heatMap.FilasHeatMapViewModel.length + gridSize + 1;
 
     var svg = d3.select("#chart").append("svg")
@@ -21,8 +24,8 @@
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var tip = d3.tip()        
-        .attr('class', 'd3-tip')   
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
         .style("visibility", "visible")
         .offset([0, 0])
         .html(function (d) {
@@ -51,26 +54,38 @@
     var offsetTextoArea = 0;
     var offsetTextoAreaAnterior = 0;
 
-    svg.selectAll(".areas")
+    var etiquetasAreas = svg.selectAll(".areas")
        .data(cantIndicadoresPorArea)
        .enter()
        .append("text")
-       .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-       .text(function (d) {  
-           var nombreArea = d[0];           
+       .attr("transform", function (d, i) {
+           if (d[1] <= 2)
+               return "translate(-6," + gridSize / 1.5 + ")";
+           else
+               return "rotate(-90)";
+       })
+       .text(function (d) {
+           var nombreArea = d[0];
            return nombreArea;
-        })       
+       })
        .attr("x", 110)
        .attr("y", function (d, i) {
-           if(i > 0){
+           if (i > 0) {
                offsetTextoArea += offsetTextoAreaAnterior;
            }
            offsetTextoAreaAnterior = d[1];
-           return (offsetTextoArea * (gridSize - 8)) - 12;
+
+           if (d[1] <= 2) {
+               return (offsetTextoArea * (gridSize - 8)) - 12;
+           }
+           else {
+               //alert(this.getComputedTextLength());               
+               return 10;
+           }
        })
        .style("text-anchor", "end")
-       .attr("class", "mono")       
-       .call(wrap, 140, -24);
+       .attr("class", "mono")
+       .call(wrap, cantIndicadoresPorArea);
 
     svg.append("line")
        .attr("x1", -30)
@@ -93,9 +108,9 @@
        .attr("y2", function (d, i) { offsetLineaY2 += d[1]; return (offsetLineaY2) * (gridSize - 8); })
        .attr("stroke-width", 0.4)
        .attr("stroke", "gray");
-    
+
     var indicadorElegido;
-    
+
     // Etiquetas tipo nombre indicadores    
     var dim1Labels = svg.selectAll(".dim1Label")
       .data(heatMap.FilasHeatMapViewModel)
@@ -103,7 +118,7 @@
         .append("a").attr("xlink:href", function (d) { return "#" }).attr("xlink:title", function (d) { return d.NombreIndicador; })
         .append("text")
         .text(function (d) {
-            if (d.NombreIndicador.length >= 50) {               
+            if (d.NombreIndicador.length >= 50) {
                 d.NombreIndicador + '...';
             }
             if (d.NombreIndicador.length >= 49) {
@@ -118,7 +133,7 @@
                 offsetYEtiqueta = 12;
             }
             return (i * (gridSize - 8)) - offsetYEtiqueta;
-        })        
+        })
         .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
         .attr("class", "mono1")
         .on('click',
@@ -126,7 +141,7 @@
 
                 if (d3.select(this).classed("mono1")) {
                     jQuery('.detalleGrafico').show();
-                    jQuery('.detalleGrafico').css("visibility","visible");                    
+                    jQuery('.detalleGrafico').css("visibility", "visible");
                 }
                 else {
                     jQuery('.detalleGrafico').toggle();
@@ -137,7 +152,7 @@
                     onDocumentReadyGauge(gauge);
 
                     var line = obtenerLineViewModel(d.Grupo);
-                    showLine(line, gauge.NombreIndicador);                    
+                    showLine(line, gauge.NombreIndicador);
                 }
 
                 if (indicadorElegido !== undefined) {
@@ -151,8 +166,8 @@
                 d3.select(this).classed("mono1", false);
                 d3.select(this).classed("text-mono1-hover", true);
             })
-            .call(wrap, 190, 127);
-   
+            .call(wrapHorizontal, 190, 127);
+
     // Etiquetas tipo meses
     var dim2Labels = svg.selectAll(".dim2Label")
         .data(heatMap.Meses)
@@ -171,7 +186,7 @@
             .attr("y", function (d) { return (d.IndiceIndicador - 1) * (gridSize - 8); })
             .attr("rx", 3) // redondeado de la celda
             .attr("ry", 3) // redondeado de la celda
-            .attr("width", gridSize +  8) // ancho de la celda
+            .attr("width", gridSize + 8) // ancho de la celda
             .attr("height", gridSize - 10) // alto de la celda
             .style("fill", "gray")
             .attr("class", "square")
@@ -189,67 +204,123 @@
 });
 
 function contarIndicadoresPorArea(heatMap) {
-    
+
     var filas = heatMap.FilasHeatMapViewModel;
-    var dictionary = {};
-    
+
     jQuery.each(filas, function (key, value) {
-        if (dictionary[value.NombreArea] != undefined) {
-            dictionary[value.NombreArea] += 1;
+        if (dictionaryIndicadoresPorArea[value.NombreArea] != undefined) {
+            dictionaryIndicadoresPorArea[value.NombreArea] += 1;
         }
         else {
-            dictionary[value.NombreArea] = 1;
+            dictionaryIndicadoresPorArea[value.NombreArea] = 1;
         }
     });
 
     var arr = [];
-    jQuery.each(dictionary, function (key, value) {
+    jQuery.each(dictionaryIndicadoresPorArea, function (key, value) {
         var arr1 = [];
         arr1.push(key);
         arr1.push(value);
-        arr.push(arr1);       
+        arr.push(arr1);
     });
 
-    return arr;    
+    return arr;
 }
 
-function wrap(text, width, posicionX) {
-
+function wrap(text, cantPorArea) {
     text.each(function () {
-        var text = d3.select(this),
-            words = text.text().split(/\s+/).reverse(),
-            word,
-            line = [],
-            lineNumber = 0,
-            lineHeight = 1.1,
-            x = text.attr("x"),
-            y = text.attr("y"),
-            dy = 0,
-            tspan = text.text(null)
-                        .append("tspan")
-                        .attr("x", posicionX)
-                        .style("text-anchor", "start")
-                        .attr("y", y)
-                        .attr("dy", dy + "em");
-
-        while (word = words.pop()) {  
-            line.push(word);
-            tspan.text(line.join(" "));
-            
-            if (tspan.node().getComputedTextLength() > width) {
-                line.pop();
-                tspan.text(line.join(" "));
-                line = [word];
-                tspan = text.append("tspan")
-                            .attr("x", posicionX)
-                            .attr("y", y)
-                            .style("text-anchor", "start")
-                            .attr("dy", ++lineNumber * lineHeight + dy + "em")
-                            .text(word);
-            }
-
+        if (dictionaryIndicadoresPorArea[d3.select(this).text()] <= 2) {
+            wrapHorizontalPalabra(d3.select(this), 90, -24);
+        }
+        else {
+            wrapVerticalPalabra(d3.select(this), 170, -25);
         }
     });
+}
+
+function wrapVertical(text, width, posicionX) {
+    text.each(function () {
+        wrapVerticalPalabra(d3.select(this), width, posicionX);
+    });
+}
+
+function wrapVerticalPalabra(text, width, posicionX) {
+
+    var words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1,
+        y = text.attr("y"),
+        dy = 0,
+        tspan = text.text(null)
+                    .append("tspan")
+                    .style("text-anchor", "start")
+                    .attr("y", y)
+                    .attr("x", 0)
+                    .attr("dx", posicionX + "em")
+                    .attr("dy", dy + "em");
+
+    while (word = words.pop()) {
+        line.push(word);
+        tspan.text(line.join(" "));
+
+        if (tspan.node().getComputedTextLength() > width) {
+            line.pop();
+            tspan.text(line.join(" "));
+            line = [word];
+            tspan = text.append("tspan")
+                        .attr("y", y)
+                        .attr("x", 0)
+                        .style("text-anchor", "start")
+                        .attr("dx", posicionX + "em")
+                        .attr("dy", ++lineNumber * lineHeight + dy + "em")
+                        .text(word);
+        }
+
+    }
+}
+
+function wrapHorizontal(text, width, posicionX) {
+    text.each(function () {
+        wrapHorizontalPalabra(d3.select(this), width, posicionX);
+    });
+}
+
+function wrapHorizontalPalabra(text, width, posicionX) {
+
+    var words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1,
+        x = text.attr("x"),
+        y = text.attr("y"),
+        dy = 0,
+        tspan = text.text(null)
+                    .append("tspan")
+                    .attr("x", posicionX)
+                    .style("text-anchor", "start")
+                    .attr("y", y)
+                    .attr("dy", dy + "em");
+
+    while (word = words.pop()) {
+        line.push(word);
+        tspan.text(line.join(" "));
+
+        if (tspan.node().getComputedTextLength() > width) {
+            line.pop();
+            tspan.text(line.join(" "));
+            line = [word];
+            tspan = text.append("tspan")
+                        .attr("x", posicionX)
+                        .attr("y", y)
+                        .style("text-anchor", "start")
+                        .attr("dy", ++lineNumber * lineHeight + dy + "em")
+                        .text(word);
+        }
+
+    }
 }
 
 function abrirGrafico(indicador) {
