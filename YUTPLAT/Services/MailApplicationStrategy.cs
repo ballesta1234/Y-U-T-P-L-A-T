69a05e9755d9eb221cb3bb6b5c8d4365f65fd017
Matias Ballestero <mballestero@cys.com.ar>
2018-05-29 15:01:15 -0300

    Configuración para C&S

--- YUTPLAT/Services/MailApplicationStrategy.cs
+++ YUTPLAT/Services/MailApplicationStrategy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.IO;
 using System.Net.Mail;
 using System.Threading.Tasks;
 
@@ -38,9 +39,31 @@ namespace YUTPLAT.Services.Interface
             {                   
                 cliente.Send(mmsg);
             }
-            catch (System.Net.Mail.SmtpException ex)
+            catch (Exception ex)
             {
+                LogException(ex);
             }
+        }
+
+        private void LogException(Exception exception)
+        {
+            string nameLog = "C:/logs/YUTPLAT_" + DateTime.Now.ToShortDateString().Replace("/", "_") + ".log";
+
+            TextWriter log = null;
+            log = new StreamWriter(nameLog, true);
+
+            log.WriteLine("");
+            log.WriteLine(DateTime.Now.ToString());
+            log.WriteLine(exception.ToString());
+            log.WriteLine("");
+
+            if (exception.InnerException != null)
+            {
+                log.WriteLine(exception.InnerException.ToString());
+                log.WriteLine("");
+            }
+
+            log.Close();
         }
     }
 }
